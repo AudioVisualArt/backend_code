0cdb8ad0bb4fdf052c85f3652779f8e943ba4271
Esteban Lemus <esteban.lemus@javeriana.edu.co>
2020-08-11 17:40:29 -0500

    conectado back con front

--- demo-master/src/main/java/com/clapp/demo/model/Product.java
+++ demo-master/src/main/java/com/clapp/demo/model/Product.java
@@ -7,9 +7,27 @@ public class Product {
 	private boolean disponible;
 	private String titulo;
 	private double valor;
+	private String id;
+	private String fotoUrl;
 	
 	
 	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getFotoUrl() {
+		return fotoUrl;
+	}
+
+	public void setFotoUrl(String fotoUrl) {
+		this.fotoUrl = fotoUrl;
+	}
+
 	public boolean isDisponible() {
 		return disponible;
 	}
