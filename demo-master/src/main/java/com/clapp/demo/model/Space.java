40a1a9dd6710a74426b3a68b47eb29984c7c96a1
Esteban Lemus <esteban.lemus@javeriana.edu.co>
2020-09-30 14:57:30 -0500

    creada clase space

---
+++ demo-master/src/main/java/com/clapp/demo/model/Space.java
@@ -0,0 +1,17 @@
+package com.clapp.demo.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Space {
+
+	String scheduleDays;
+	String scheduleHours;
+	String name;
+	String Location;
+	int rating;
+	String userOwner;
+	String description;
+	int minimumHours;
+	long priceHour;
+}
