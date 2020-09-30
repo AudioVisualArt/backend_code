2ae10407b492141ad0f357bd0cba7fe7aa318022
Esteban Lemus <esteban.lemus@javeriana.edu.co>
2020-09-30 15:29:56 -0500

    SpaceController creado

---
+++ demo-master/src/main/java/com/clapp/demo/cont/SpaceController.java
@@ -0,0 +1,5 @@
+package com.clapp.demo.cont;
+
+public class SpaceController {
+
+}
--- demo-master/src/main/java/com/clapp/demo/model/Space.java
+++ demo-master/src/main/java/com/clapp/demo/model/Space.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Space {
 
+	String id;
 	String scheduleDays;
 	String scheduleHours;
 	String name;
@@ -14,4 +15,69 @@ public class Space {
 	String description;
 	int minimumHours;
 	long priceHour;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getScheduleDays() {
+		return scheduleDays;
+	}
+	public void setScheduleDays(String scheduleDays) {
+		this.scheduleDays = scheduleDays;
+	}
+	public String getScheduleHours() {
+		return scheduleHours;
+	}
+	public void setScheduleHours(String scheduleHours) {
+		this.scheduleHours = scheduleHours;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public String getUserOwner() {
+		return userOwner;
+	}
+	public void setUserOwner(String userOwner) {
+		this.userOwner = userOwner;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getMinimumHours() {
+		return minimumHours;
+	}
+	public void setMinimumHours(int minimumHours) {
+		this.minimumHours = minimumHours;
+	}
+	public long getPriceHour() {
+		return priceHour;
+	}
+	public void setPriceHour(long priceHour) {
+		this.priceHour = priceHour;
+	}
+	
+	
 }
